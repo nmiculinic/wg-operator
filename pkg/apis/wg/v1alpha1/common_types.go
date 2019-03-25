210d4953aa29d2e6706de801e34ec63a177dab44
Neven Miculinic <neven.miculinic@gmail.com>
2019-03-25 17:53:18 +0100

    Added client wgtype config + parsing

--- pkg/apis/wg/v1alpha1/common_types.go
+++ pkg/apis/wg/v1alpha1/common_types.go
@@ -4,5 +4,4 @@ type CommonSpec struct {
 	PublicKey string `json:"publicKey"`
 	Address string `json:"address"`
 	ExtraAllowedIPs []string `json:"extraAllowedIPs"`
-	ListenPort int `json:"listenPort"`
 }
--- pkg/wgctl/config.go
+++ pkg/wgctl/config.go
@@ -2,11 +2,54 @@ package wgctl
 
 import (
 	"bytes"
+	"github.com/Masterminds/sprig"
+	"encoding/base64"
 	"fmt"
 	"github.com/KrakenSystems/wg-operator/pkg/apis/wg/v1alpha1"
+	"github.com/mdlayher/wireguardctrl/wgtypes"
 	"net"
 	"text/template"
 )
+
+type Config struct {
+	wgtypes.Config
+	Address net.IP
+}
+
+// Address = {{ .Client.Spec.Address }}
+const wgtypeTemplateSpec =
+	`[Interface]
+Address = {{ .Address }}
+PrivateKey = {{ .PrivateKey | wgKey }}
+{{- if .ListenPort }}ListenPort = {{ .ListenPort }}{{ end }}
+{{- range .Peers }}
+
+[Peer]
+PublicKey = {{ .PublicKey | wgKey }}
+AllowedIps = {{ range $i, $el := .AllowedIPs }}{{if $i}}, {{ end }}{{ $el }}{{ end }}
+Endpoint = {{ .Endpoint }}
+{{- end }}
+`
+
+func serializeKey(key *wgtypes.Key) string {
+	return base64.StdEncoding.EncodeToString(key[:])
+}
+
+var cfgTemplate = template.Must(
+	template.
+		New("wg-cfg").
+		Funcs(sprig.HermeticTxtFuncMap()).
+		Funcs(template.FuncMap(map[string]interface{}{"wgKey": serializeKey})).
+		Parse(wgtypeTemplateSpec))
+
+
+func SerializeConfig(cfg *Config) (string, error) {
+	buff := &bytes.Buffer{}
+	if err := cfgTemplate.Execute(buff, cfg); err != nil {
+		return "", err
+	}
+	return buff.String(), nil
+}
 
 type ClientRequest struct {
 	PrivateKey string
@@ -28,32 +71,68 @@ func (c *ClientRequest) Validate() error {
 	return nil
 }
 
-const clientTemplateSpec =
-`[Interface]
-Address = {{ .Client.Spec.Address }}
-PrivateKey = {{ .PrivateKey }}
-ListenPort = {{ .Client.Spec.ListenPort }}
-{{- range .Servers.Items }}
-
-[Peer]
-PublicKey = {{ .Spec.PublicKey }}
-AllowedIps = {{ .Spec.Address }}/32 {{- range .Spec.ExtraAllowedIPs }}, {{ . }}{{- end }}
-Endpoint = {{ .Spec.Endpoint}}:{{ .Spec.ListenPort }}
-{{- end }}
-`
-var clientTemplate = template.Must(template.New("client").Parse(clientTemplateSpec))
 
 
-func CreateClientConfig(req ClientRequest) (string, error) {
+func CreateClientConfig(req ClientRequest) (*Config, error) {
 	if err := req.Validate(); err != nil {
-		return "", err
+		return nil, err
 	}
 
-	buff := &bytes.Buffer{}
-	if err := clientTemplate.Execute(buff, req); err != nil {
-		return "", err
+	pkeySlice, err := base64.StdEncoding.DecodeString(req.PrivateKey)
+	if err != nil {
+		return nil, err
 	}
-	return buff.String(), nil
+
+	var pkey wgtypes.Key
+	copy(pkey[:], pkeySlice[:])
+
+	cfg := Config{
+		Address: net.ParseIP(req.Client.Spec.Address),
+		Config: wgtypes.Config{
+			PrivateKey: &pkey,
+			ReplacePeers:true,
+			Peers:make([]wgtypes.PeerConfig, len(req.Servers.Items)),
+		},
+	}
+	if cfg.Address == nil {
+		return nil, fmt.Errorf("cannot parse IP %v", req.Client.Spec.Address)
+	}
+
+	for i, srv := range req.Servers.Items {
+		pkeySlice, err := base64.StdEncoding.DecodeString(srv.Spec.PublicKey)
+		if err != nil {
+			return nil, err
+		}
+		var srvKey wgtypes.Key
+		copy(srvKey[:], pkeySlice[:])
+
+		ep, err := net.ResolveUDPAddr("", srv.Spec.Endpoint)
+		if err != nil {
+			return nil, err
+		}
+
+		cfg.Peers[i] = wgtypes.PeerConfig{
+			ReplaceAllowedIPs:true,
+			PublicKey: srvKey,
+			Endpoint: ep,
+			AllowedIPs:make([]net.IPNet, 0, 1 + len(srv.Spec.ExtraAllowedIPs)),
+		}
+
+		_, c, err := net.ParseCIDR(srv.Spec.Address + "/32")
+		if err != nil {
+			return nil, err
+		}
+		cfg.Peers[i].AllowedIPs = append(cfg.Peers[i].AllowedIPs, *c)
+		for _, cidr := range srv.Spec.ExtraAllowedIPs {
+			_, c, err := net.ParseCIDR(cidr)
+			if err != nil {
+				return nil, err
+			}
+			cfg.Peers[i].AllowedIPs = append(cfg.Peers[i].AllowedIPs, *c)
+		}
+
+	}
+	return &cfg, nil
 }
 
 type ServerRequest struct {
