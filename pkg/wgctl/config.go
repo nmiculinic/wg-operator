973a49bb72356cc62ec45635153bdfbe73019d84
Neven Miculinic <neven.miculinic@gmail.com>
2019-03-25 18:27:57 +0100

    refacted to use wgtypes under the hood

--- pkg/wgctl/config.go
+++ pkg/wgctl/config.go
@@ -21,18 +21,29 @@ const wgtypeTemplateSpec =
 	`[Interface]
 Address = {{ .Address }}
 PrivateKey = {{ .PrivateKey | wgKey }}
-{{- if .ListenPort }}ListenPort = {{ .ListenPort }}{{ end }}
+{{- if .ListenPort }}{{ "\n" }}ListenPort = {{ .ListenPort }}{{ end }}
 {{- range .Peers }}
 
 [Peer]
 PublicKey = {{ .PublicKey | wgKey }}
 AllowedIps = {{ range $i, $el := .AllowedIPs }}{{if $i}}, {{ end }}{{ $el }}{{ end }}
-Endpoint = {{ .Endpoint }}
+{{- if .Endpoint }}{{ "\n" }}Endpoint = {{ .Endpoint }}{{ end }}
+
 {{- end }}
 `
 
 func serializeKey(key *wgtypes.Key) string {
 	return base64.StdEncoding.EncodeToString(key[:])
+}
+
+func parseKey(key string) (wgtypes.Key,error) {
+	var pkey wgtypes.Key
+	pkeySlice, err := base64.StdEncoding.DecodeString(key)
+	if err != nil {
+		return pkey, err
+	}
+	copy(pkey[:], pkeySlice[:])
+	return pkey, nil
 }
 
 var cfgTemplate = template.Must(
@@ -51,87 +62,94 @@ func SerializeConfig(cfg *Config) (string, error) {
 	return buff.String(), nil
 }
 
+func commonPeerConfig(common v1alpha1.CommonSpec) (wgtypes.PeerConfig, error) {
+	srvKey, err := parseKey(common.PublicKey)
+
+	peer := wgtypes.PeerConfig{
+		ReplaceAllowedIPs:true,
+		PublicKey: srvKey,
+		AllowedIPs:make([]net.IPNet, 0, 1 + len(common.ExtraAllowedIPs)),
+	}
+
+	_, c, err := net.ParseCIDR(common.Address + "/32")
+	if err != nil {
+		return wgtypes.PeerConfig{}, err
+	}
+	peer.AllowedIPs = append(peer.AllowedIPs, *c)
+	for _, cidr := range common.ExtraAllowedIPs {
+		_, c, err := net.ParseCIDR(cidr)
+		if err != nil {
+			return wgtypes.PeerConfig{}, err
+		}
+		peer.AllowedIPs = append(peer.AllowedIPs, *c)
+	}
+
+	return peer, nil
+}
+
+func ServerPeers(servers []v1alpha1.Server, skipServer string) ([]wgtypes.PeerConfig, error) {
+	peers := make([]wgtypes.PeerConfig, 0, len(servers))
+	for _, srv := range servers {
+		if srv.Name == skipServer {
+			continue
+		}
+		peer, err := commonPeerConfig(srv.Spec.CommonSpec)
+		if err != nil {
+			return nil, err
+		}
+		ep, err := net.ResolveUDPAddr("", srv.Spec.Endpoint)
+		if err != nil {
+			return nil, err
+		}
+		peer.Endpoint = ep
+		peers = append(peers, peer)
+	}
+	return peers, nil
+}
+
+func ClientPeers(servers []v1alpha1.Client, skipClient string) ([]wgtypes.PeerConfig, error) {
+	peers := make([]wgtypes.PeerConfig, 0, len(servers))
+	for _, srv := range servers {
+		if srv.Name == skipClient {
+			continue
+		}
+		peer, err := commonPeerConfig(srv.Spec.CommonSpec)
+		if err != nil {
+			return nil, err
+		}
+		peers = append(peers, peer)
+	}
+	return peers, nil
+}
+
 type ClientRequest struct {
 	PrivateKey string
 	Client v1alpha1.Client
 	Servers v1alpha1.ServerList
 }
 
-func (c *ClientRequest) Validate() error {
-	if net.ParseIP(c.Client.Spec.Address) == nil {
-		return fmt.Errorf("invalid client ip")
-	}
-	for _, srv := range c.Servers.Items {
-		for _, addr := range srv.Spec.ExtraAllowedIPs {
-			if _, _, err := net.ParseCIDR(addr); err != nil {
-				return fmt.Errorf("cannot parse CIDR %s for server %s", addr, srv.Name)
-			}
-		}
-	}
-	return nil
-}
-
-
-
 func CreateClientConfig(req ClientRequest) (*Config, error) {
-	if err := req.Validate(); err != nil {
-		return nil, err
-	}
-
-	pkeySlice, err := base64.StdEncoding.DecodeString(req.PrivateKey)
+	key, err := parseKey(req.PrivateKey)
 	if err != nil {
 		return nil, err
 	}
 
-	var pkey wgtypes.Key
-	copy(pkey[:], pkeySlice[:])
-
+	peers, err := ServerPeers(req.Servers.Items, "")
+	if err != nil {
+		return nil, err
+	}
 	cfg := Config{
 		Address: net.ParseIP(req.Client.Spec.Address),
 		Config: wgtypes.Config{
-			PrivateKey: &pkey,
+			PrivateKey: &key,
 			ReplacePeers:true,
-			Peers:make([]wgtypes.PeerConfig, len(req.Servers.Items)),
+			Peers: peers,
 		},
 	}
 	if cfg.Address == nil {
 		return nil, fmt.Errorf("cannot parse IP %v", req.Client.Spec.Address)
 	}
 
-	for i, srv := range req.Servers.Items {
-		pkeySlice, err := base64.StdEncoding.DecodeString(srv.Spec.PublicKey)
-		if err != nil {
-			return nil, err
-		}
-		var srvKey wgtypes.Key
-		copy(srvKey[:], pkeySlice[:])
-
-		ep, err := net.ResolveUDPAddr("", srv.Spec.Endpoint)
-		if err != nil {
-			return nil, err
-		}
-
-		cfg.Peers[i] = wgtypes.PeerConfig{
-			ReplaceAllowedIPs:true,
-			PublicKey: srvKey,
-			Endpoint: ep,
-			AllowedIPs:make([]net.IPNet, 0, 1 + len(srv.Spec.ExtraAllowedIPs)),
-		}
-
-		_, c, err := net.ParseCIDR(srv.Spec.Address + "/32")
-		if err != nil {
-			return nil, err
-		}
-		cfg.Peers[i].AllowedIPs = append(cfg.Peers[i].AllowedIPs, *c)
-		for _, cidr := range srv.Spec.ExtraAllowedIPs {
-			_, c, err := net.ParseCIDR(cidr)
-			if err != nil {
-				return nil, err
-			}
-			cfg.Peers[i].AllowedIPs = append(cfg.Peers[i].AllowedIPs, *c)
-		}
-
-	}
 	return &cfg, nil
 }
 
@@ -142,61 +160,39 @@ type ServerRequest struct {
 	Servers v1alpha1.ServerList
 }
 
-func (c *ServerRequest) Validate() error {
-	if net.ParseIP(c.Me.Spec.Address) == nil {
-		return fmt.Errorf("invalid client ip")
-	}
-	for _, srv := range c.Servers.Items {
-		for _, addr := range srv.Spec.ExtraAllowedIPs {
-			if _, _, err := net.ParseCIDR(addr); err != nil {
-				return fmt.Errorf("cannot parse CIDR %s for server %s", addr, srv.Name)
-			}
-		}
-		if srv.Spec.Address == c.Me.Spec.Address {
-			return fmt.Errorf("me: [%s] has same address as server %s -> %s", c.Me.Name, srv.Name, srv.Spec.Address)
-		}
+func CreateServerConfig(req ServerRequest) (*Config, error) {
+	key, err := parseKey(req.PrivateKey)
+	if err != nil {
+		return nil, err
 	}
 
-	for _, client := range c.Clients.Items {
-		for _, addr := range client.Spec.ExtraAllowedIPs {
-			if _, _, err := net.ParseCIDR(addr); err != nil {
-				return fmt.Errorf("cannot parse CIDR %s for client %s", addr, client.Name)
-			}
-		}
-	}
-	return nil
-}
-
-const serverTemplateSpec =
-	`[Interface]
-Address = {{ .Me.Spec.Address }}
-PrivateKey = {{ .PrivateKey }}
-ListenPort = {{ .Me.Spec.ListenPort }}
-{{- range .Servers.Items }}
-
-[Peer]
-PublicKey = {{ .Spec.PublicKey }}
-AllowedIps = {{ .Spec.Address }}/32 {{- range .Spec.ExtraAllowedIPs }}, {{ . }}{{- end }}
-Endpoint = {{ .Spec.Endpoint}}:{{ .Spec.ListenPort }}
-{{- end }}
-{{- range .Clients.Items }}
-
-[Peer]
-PublicKey = {{ .Spec.PublicKey }}
-AllowedIps = {{ .Spec.Address }}/32 {{- range .Spec.ExtraAllowedIPs }}, {{ . }}{{- end }}
-{{- end }}
-`
-var serverTemplate = template.Must(template.New("server").Parse(serverTemplateSpec))
-
-func CreateServerConfig(req ServerRequest) (string, error) {
-	if err := req.Validate(); err != nil {
-		return "", err
+	ep, err := net.ResolveUDPAddr("", req.Me.Spec.Endpoint)
+	if err != nil {
+		return nil, err
 	}
 
-	buff := &bytes.Buffer{}
-	if err := serverTemplate.Execute(buff, req); err != nil {
-		return "", err
+	serverPeers, err := ServerPeers(req.Servers.Items, req.Me.Name)
+	if err != nil {
+		return nil, err
 	}
-	return buff.String(), nil
+
+	clientPeers, err := ClientPeers(req.Clients.Items, "")
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := Config{
+		Address: net.ParseIP(req.Me.Spec.Address),
+		Config: wgtypes.Config{
+			PrivateKey: &key,
+			ReplacePeers:true,
+			Peers: append(serverPeers, clientPeers...),
+			ListenPort: &ep.Port,
+		},
+	}
+	if cfg.Address == nil {
+		return nil, fmt.Errorf("cannot parse IP %v", req.Me.Spec.Address)
+	}
+	return &cfg, nil
 }
 
--- pkg/wgctl/config_test.go
+++ pkg/wgctl/config_test.go
@@ -1,7 +1,6 @@
 package wgctl
 
 import (
-	"fmt"
 	"github.com/KrakenSystems/wg-operator/pkg/apis/wg/v1alpha1"
 	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -16,6 +15,7 @@ type clientTestCase struct {
 
 var (
 	cl1 = v1alpha1.Client{
+		ObjectMeta: v1.ObjectMeta{Name:"client-cl1"},
 	Spec: v1alpha1.ClientSpec{
 		CommonSpec: v1alpha1.CommonSpec{
 			PublicKey: "rh3vMGPqe6UhQwly7kZKYAvG4tJa7+j5lOPICXI/1kI=",
@@ -25,6 +25,7 @@ var (
 	}
 
 	cl2 = v1alpha1.Client{
+		ObjectMeta: v1.ObjectMeta{Name:"client-cl2"},
 		Spec: v1alpha1.ClientSpec{
 			CommonSpec: v1alpha1.CommonSpec{
 				PublicKey: "VssQj+7Qxa4e8Ar9i2lr9hs1U6SagOf3kTLP+Mj4HVA=",
@@ -33,6 +34,7 @@ var (
 		},
 	}
 	server1 = v1alpha1.Server{
+		ObjectMeta: v1.ObjectMeta{Name:"server1"},
 		Spec:v1alpha1.ServerSpec{
 			CommonSpec:v1alpha1.CommonSpec{
 				PublicKey: "qlgnbDFeqmA/qbxbtol4mYB0Eq/rDNfJA7Wg97mJ2Vs=",
@@ -43,6 +45,7 @@ var (
 	}
 
 	server2 = v1alpha1.Server{
+		ObjectMeta: v1.ObjectMeta{Name:"server2"},
 		Spec:v1alpha1.ServerSpec{
 			CommonSpec:v1alpha1.CommonSpec{
 				PublicKey: "W+4V89h8brHTGM2/iqNItkuRWuCVYfBrl4lKGjW/zCg=",
@@ -64,6 +67,7 @@ var (
 	}
 
 	clNoIP = v1alpha1.Client{
+		ObjectMeta: v1.ObjectMeta{Name:"client-cl-no-ip"},
 		Spec: v1alpha1.ClientSpec{
 			CommonSpec: v1alpha1.CommonSpec{
 				PublicKey: "RZbjqfNqvnl14BwxPEvjeFMZje2TUfg6POL93Okr3DQ=",
@@ -184,37 +188,41 @@ func TestServer(t *testing.T) {
 		"no-server": {
 			req: ServerRequest{
 					Me:         server3,
-					PrivateKey: "PRIVATE_KEY",
+					PrivateKey: "QBNloaEPjZd/nafQcH55kdYqnQ6YB6gX35l//QGra2E=",
 					Servers: v1alpha1.ServerList{Items: []v1alpha1.Server{server3}},
 					Clients: v1alpha1.ClientList{},
 				},
-			targetConfig: "",
-			targetError:fmt.Errorf("me: [server3] has same address as server server3 -> 10.100.2.1"),
+			targetConfig: `[Interface]
+Address = 10.100.2.1
+PrivateKey = QBNloaEPjZd/nafQcH55kdYqnQ6YB6gX35l//QGra2E=
+ListenPort = 123
+`,
+			targetError:nil,
 		},
 		"2server-2clients": {
 			req: ServerRequest{
 				Me:         server3,
-				PrivateKey: "PRIVATE_KEY",
+				PrivateKey: "QBNloaEPjZd/nafQcH55kdYqnQ6YB6gX35l//QGra2E=",
 				Servers: v1alpha1.ServerList{Items: []v1alpha1.Server{server1}},
 				Clients: v1alpha1.ClientList{Items:[]v1alpha1.Client{cl1, cl2}},
 			},
 			targetConfig:
 				`[Interface]
 Address = 10.100.2.1
-PrivateKey = PRIVATE_KEY
+PrivateKey = QBNloaEPjZd/nafQcH55kdYqnQ6YB6gX35l//QGra2E=
 ListenPort = 123
 
 [Peer]
-PublicKey = pub-key-server1
+PublicKey = qlgnbDFeqmA/qbxbtol4mYB0Eq/rDNfJA7Wg97mJ2Vs=
 AllowedIps = 10.100.1.1/32
 Endpoint = 35.12.23.34:555
 
 [Peer]
-PublicKey = pub-key-cl1
+PublicKey = rh3vMGPqe6UhQwly7kZKYAvG4tJa7+j5lOPICXI/1kI=
 AllowedIps = 10.100.0.1/32
 
 [Peer]
-PublicKey = pub-key-cl2
+PublicKey = VssQj+7Qxa4e8Ar9i2lr9hs1U6SagOf3kTLP+Mj4HVA=
 AllowedIps = 10.100.0.2/32
 `,
 		},
@@ -222,17 +230,26 @@ AllowedIps = 10.100.0.2/32
 	for name , testCase := range tbl {
 		t.Run(name, func(t *testing.T) {
 			result, err := CreateServerConfig(testCase.req)
+			t.Log(result)
 			if err != nil {
 				t.Log("err", err.Error())
+				if testCase.targetError != nil {
+					assert.Equal(t, testCase.targetError.Error(), err.Error())
+				} else {
+					t.Fail()
+				}
 			}
 
 			if testCase.targetError == nil {
 				assert.NoError(t, err)
 			} else {
 				assert.Error(t, err)
-				assert.Equal(t, testCase.targetError.Error(), err.Error())
 			}
-			assert.Equal(t, testCase.targetConfig, result)
+
+			cfg, err := SerializeConfig(result)
+			t.Logf("Serialized config:\n%v", cfg)
+			assert.NoError(t, err)
+			assert.Equal(t, testCase.targetConfig, cfg)
 		})
 	}
 }
